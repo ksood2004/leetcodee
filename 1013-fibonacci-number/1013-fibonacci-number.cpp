db670b15adc6034dd81061310fd8b398be547a4f
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2024-11-02 15:05:40 +0530

    Time: 16 ms (16.51%), Space: 7.3 MB (89.82%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,24 +1,13 @@
-class Solution
-{
+class Solution {
 public:
-	int fib(int n)
-	{
+    int fib(int n) {
         if(n==0){
             return 0;
         }
-        else if(n==1){
+        if(n==1){
             return 1;
         }
+        return fib(n-1)+fib(n-2);
         
-		int n1=0;
-        int n2=1;
-        int n3;
-        for(int i=2;i<=n;i++){
-            n3=n1+n2;
-            n1=n2;
-            n2=n3;
-
-        }
-        return n3;
-	}
+    }
 };
