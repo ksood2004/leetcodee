2332351cfb79caced72c00410aadc8d0cc525532
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2024-11-13 17:33:26 +0530

    Time: 0 ms (100%), Space: 27.1 MB (86.78%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
-    void reverseString(vector<char>& s) {
-        int start=0;
-        int end=s.size()-1;;
-        while(start<end){
-            swap(s[start++],s[end--]);
+void rev(vector<char>& s,int i,int j){
+        if(i>=j){
+            return ;
         }
+        swap(s[i],s[j]);
+
+        rev(s,i+1,j-1);
+
+}
+    void reverseString(vector<char>& s) {
+        rev(s,0,s.size()-1);
         
     }
 };
