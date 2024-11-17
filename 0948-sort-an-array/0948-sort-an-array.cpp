163715d7e5c097e5171c2622c81b740bc1862f57
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2024-11-17 12:59:57 +0530

    Time: 291 ms (43.02%), Space: 176.1 MB (33.09%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.cpp
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -1,72 +1,61 @@
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
-    // Merge function to merge two sorted halves
-    void mergesor(vector<int>& arr, int s, int e) {
-        int mid = s + (e - s) / 2;
-        int len1 = mid - s + 1;
-        int len2 = e - mid;
+    void mergesort(vector<int>&arr,int s,int e){
+        int mid=s+(e-s)/2;
+        int len1=mid-s+1;
+        int len2=e-mid;
 
-        // Temporary arrays to store the two halves
-        int *first = new int[len1];
-        int *second = new int[len2];
+        int *arr1=new int[len1];
+        int *arr2=new int[len2];
 
-        // Copy values into temp arrays
-        int index = s;
-        for (int i = 0; i < len1; i++) {
-            first[i] = arr[index++];
+        int index=s;
+        for(int i=0;i<len1;i++){
+            arr1[i]=arr[index++];
         }
 
-        index = mid + 1;
-        for (int i = 0; i < len2; i++) {
-            second[i] = arr[index++];
+        index=mid+1;
+        for(int i=0;i<len2;i++){
+            arr2[i]=arr[index++];
         }
 
-        // Merge the two sorted halves back into the main array
-        int ind1 = 0, ind2 = 0;
-        index = s;
+        int ind1=0;
+        int ind2=0;
+        index=s;
 
-        while (ind1 < len1 && ind2 < len2) {
-            if (first[ind1] <= second[ind2]) {
-                arr[index++] = first[ind1++];
-            } else {
-                arr[index++] = second[ind2++];
+        while(ind1<len1 && ind2<len2){
+            if(arr1[ind1]<=arr2[ind2]){
+                arr[index++]=arr1[ind1++];
             }
+            else{
+                arr[index++]=arr2[ind2++];
+            }
+
         }
 
-        // Copy any remaining elements from first and second arrays
-        while (ind1 < len1) {
-            arr[index++] = first[ind1++];
+        while(ind1<len1){
+            arr[index++]=arr1[ind1++];
         }
-        while (ind2 < len2) {
-            arr[index++] = second[ind2++];
-        }
+        while(ind2<len2){
+             arr[index++]=arr2[ind2++];
+        } 
+        delete[] arr1;
+        delete[] arr2;       
+}
 
-        // Free the allocated memory
-        delete[] first;
-        delete[] second;
+void ms(vector<int>&arr,int s,int e){
+    if(s>=e){
+        return ;
     }
+    int mid=s+(e-s)/2;
 
-    // Merge sort function to divide the array
-    void merge(vector<int>& arr, int s, int e) {
-        if (s >= e) {
-            return;
-        }
-        int mid = s + (e - s) / 2;
-
-        // Recursively split the array into halves
-        merge(arr, s, mid);
-        merge(arr, mid + 1, e);
-
-        // Merge the two sorted halves
-        mergesor(arr, s, e);
-    }
-
-    // Function to sort the array
+    ms(arr,s,mid);
+    ms(arr,mid+1,e);
+    mergesort(arr,s,e);
+}
     vector<int> sortArray(vector<int>& nums) {
-        merge(nums, 0, nums.size() - 1);
+
+        ms(nums,0,nums.size()-1);
         return nums;
+        
     }
 };
