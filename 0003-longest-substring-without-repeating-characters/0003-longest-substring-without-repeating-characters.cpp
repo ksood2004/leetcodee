5c9cddbd77dbafbf42f96ee2529bf8cf53c7dfdd
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2024-12-16 15:51:48 +0530

    Time: 11 ms (58.12%), Space: 13.7 MB (40.55%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int l = 0;  // Left pointer of the sliding window
-        int res = 0;  // Variable to store the result
-        unordered_set<char> hello;  // Set to store characters in the current window
-        
-        for (int i = 0; i < s.size(); ++i) {
-            // If the character at 'i' is already in the set, move the left pointer
-            while (hello.find(s[i]) != hello.end()) {
-                hello.erase(s[l]);  // Remove character at 'l' from the set
-                l++;  // Move the left pointer to the right
+          int left=0,right=0,ans=0;
+        int n=s.size();
+        map<char,int>freq;
+        int count=0;
+        while(right<n){
+            freq[s[right]]++;
+            count++;
+            
+            if(freq.size()==count){
+                ans=max(ans,right-left+1);
             }
-            // Add the current character to the set
-            hello.insert(s[i]);
-            // Update the result with the maximum length of the current window
-            res = max(res, i - l + 1);
+            else{
+                while(freq.size()<count){
+                    freq[s[left]]--;
+                    count--;
+                    if(freq[s[left]]==0){
+                        freq.erase(s[left]);
+                    }
+                    left++;
+                }
+            }
+            right++;
         }
         
-        return res;
+        return ans;
+        
     }
+    
 };
