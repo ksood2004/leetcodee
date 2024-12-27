236c808f186faebc0088e6d25fdc709f71f08de6
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2024-12-27 18:09:50 +0530

    Time: 2 ms (42.58%), Space: 15.8 MB (6.13%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,25 +1,23 @@
 class Solution {
-    private:
-    void subb(vector<int>output,vector<vector<int>> &ans,int index,vector<int>nums){
-        if(index>=nums.size()){
-            ans.push_back(output);
-            return ;
-        }
-
-        subb(output,ans,index+1,nums);
-        int karan=nums[index];
-        output.push_back(karan);
-        subb(output,ans,index+1,nums);
-    }
 public:
+void solve(vector<int>& ip, vector<int>& op,vector<vector<int>>& opp){
+    if(ip.size()==0){
+        opp.push_back(op);
+        return;
+    }
+    vector<int> op1=op;
+    vector<int> op2=op;
+    op2.push_back(ip[0]);
+    vector<int>erasee(ip.begin()+1,ip.end());
+    solve(erasee,op1,opp);
+    solve(erasee,op2,opp);
+    
+}
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans;
-        vector<int> output;
-        int index=0;
-        subb(output,ans,index,nums);
-        return ans;
-
-
+         vector<vector<int>> result; // To store all subsets
+        vector<int> op; // To store the current subset
+        solve(nums, op, result);
+        return result;
         
     }
 };
