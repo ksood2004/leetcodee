323c3a019b85095b9e932a0c0150fa6f77edba5c
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2024-12-29 20:02:44 +0530

    Time: 22 ms (27.17%), Space: 26.4 MB (9.3%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
-void solve(int n, int k , int & ans,vector<int> person,int index){
-    if(person.size()==1){
-        ans=person[0];
-        return;
-    }
-    index=(index+k)%person.size();
-    person.erase(person.begin()+index);
-    solve(n,k,ans,person,index);
-}
+// void solve(int n, int k , int & ans,vector<int> person,int index){
+//     if(person.size()==1){
+//         ans=person[0];
+//         return;
+//     }
+//     index=(index+k)%person.size();
+//     person.erase(person.begin()+index);
+//     solve(n,k,ans,person,index);
+// }
     int findTheWinner(int n, int k) {
-        int ans=-1;
-        k=k-1;
-        vector<int> karan;
-        for(int i=0;i<n;i++){
-            karan.push_back(i+1);
+        queue<int> karan;
+        for(int i=1;i<=n;i++){
+        karan.push(i);
         }
-        solve(n,k,ans,karan,0);
-        return ans;
+        while(karan.size()>1){
+            for(int i=1;i<k;i++){
+                karan.push(karan.front());
+                karan.pop();
+            }
+            karan.pop();
+        }
+        return karan.front();
         
     }
 };
