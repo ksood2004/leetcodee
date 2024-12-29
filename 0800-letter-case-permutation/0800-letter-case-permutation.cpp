b9ea69c1eccfaba63df6a61c3af6b8e0d6e625b7
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2024-12-29 10:35:14 +0530

    Time: 4 ms (50.5%), Space: 15.3 MB (46.95%) - LeetHub

---
+++ 0800-letter-case-permutation/0800-letter-case-permutation.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void solve(string ip, string op, vector<string>& result) {
+        if (ip.length() == 0) {
+            result.push_back(op);
+            return;
+        }
+        // agar character hai toh
+        if (isalpha(ip[0])) {
+            
+            string op1 = op;
+            string op2 = op;
+            op1.push_back(tolower(ip[0]));//lower character
+            op2.push_back(toupper(ip[0])); // upper character
+            ip.erase(ip.begin());
+            solve(ip, op1, result);
+            solve(ip, op2, result);
+        } else {
+            // If the current character is not alphabetic, simply add it to output
+            op.push_back(ip[0]);
+            ip.erase(ip.begin());
+            solve(ip, op, result);
+        }
+    }
+
+    vector<string> letterCasePermutation(string s) {
+        vector<string> result;
+        string op;
+        solve(s, op, result);
+        return result;
+    }
+};
