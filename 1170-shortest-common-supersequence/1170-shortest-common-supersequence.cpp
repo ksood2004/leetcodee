1c6e59b1ee71d0909f25501f254da0068bf36283
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-01-13 18:17:21 +0530

    Time: 23 ms (87.2%), Space: 17.4 MB (93.02%) - LeetHub

---
+++ 1170-shortest-common-supersequence/1170-shortest-common-supersequence.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    string shortestCommonSupersequence(string str1, string str2) {
+        int n = str1.size();
+        int m = str2.size();
+        int t[n + 1][m + 1];
+        
+        // Fill the LCS DP table
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                if (i == 0 || j == 0) {
+                    t[i][j] = 0;
+                } else if (str1[i - 1] == str2[j - 1]) {
+                    t[i][j] = 1 + t[i - 1][j - 1];
+                } else {
+                    t[i][j] = max(t[i - 1][j], t[i][j - 1]);
+                }
+            }
+        }
+
+        string result = "";
+        int i = n, j = m;
+        while (i > 0 && j > 0) {
+            if (str1[i - 1] == str2[j - 1]) {
+                result = str1[i - 1] + result;
+                i--;
+                j--;
+            } else if (t[i - 1][j] > t[i][j - 1]) {
+                result = str1[i - 1] + result;
+                i--;
+            } else {
+                result = str2[j - 1] + result;
+                j--;
+            }
+        }
+
+        while (i > 0) {
+            result = str1[i - 1] + result;
+            i--;
+        }
+        while (j > 0) {
+            result = str2[j - 1] + result;
+            j--;
+        }
+        
+        return result;
+    }
+};
