b541b80f4bfae350849a656e98f44b50a76c6b27
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-02-12 18:28:15 +0530

    Time: 0 ms (100%), Space: 24.5 MB (29.41%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -1,25 +1,23 @@
-#include <queue>
-using namespace std;
-
 class Solution {
 public:
-    void inorder(TreeNode* root, priority_queue<int>& pq, int k) {
-        if (!root) return;
-
-        inorder(root->left, pq, k);
-
-        pq.push(root->val);
-
-        if (pq.size() > k) {
-            pq.pop();
-        }
-
-        inorder(root->right, pq, k);
-    }
-
     int kthSmallest(TreeNode* root, int k) {
-        priority_queue<int> pq; 
-        inorder(root, pq, k);
-        return pq.top(); 
+        stack<TreeNode*> st;
+        while (true) {
+            // Go to the leftmost node
+            while (root) {
+                st.push(root);
+                root = root->left;
+            }
+            
+            // Process node
+            root = st.top();
+            st.pop();
+            k--;
+
+            if (k == 0) return root->val;
+
+            // Move to the right subtree
+            root = root->right;
+        }
     }
 };
