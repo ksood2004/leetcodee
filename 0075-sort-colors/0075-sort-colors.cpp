7f5d5d6723c8ec1af1fee61e75b499179cbac95b
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-06-23 20:59:00 +0530

    Time: 0 ms (100%), Space: 11.7 MB (12.54%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        unordered_map<int,int> karan={{0,0},{1,0},{2,0}};
-        for(int num: nums){
-            karan[num]++;
-        }
         int n=nums.size();
-        int idx=0;
+        unordered_map<int,int> karan={{0,0},{1,0},{2,0}};
+        for(int i=0;i<n;i++){
+            karan[nums[i]]++;
+        }
+        int index=0;
         for(int i=0;i<3;i++){
             int freq=karan[i];
             for(int j=0;j<freq;j++){
-                nums[idx]=i;
-                idx++;
+                nums[index]=i;
+                index++;
             }
         }
+        
     }
 };
