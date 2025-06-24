c5d2b0cc6c1a08110569611c66bacc5618779e29
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-06-24 17:40:34 +0530

    Time: 0 ms (100%), Space: 15.1 MB (66.27%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,52 +1,48 @@
 class Solution {
 public:
-int pivott(vector<int>& nums,int n){
-   
+int pivott(vector<int>&nums){
+    int n=nums.size();
         int s=0;
         int e=n-1;
-        int mid;
         while(s<e){
-            mid=s+(e-s)/2;
+            int mid=s+(e-s)/2;
             if(nums[mid]>=nums[0]){
                 s=mid+1;
             }
             else{
                 e=mid;
             }
-           
         }
-    return s;      
+        return s;
 }
 
 int bs(vector<int>& nums, int target,int s,int e){
-    
-    
-    while(s<=e){
-        int mid=s+(e-s)/2;
-    if(nums[mid]==target){
-        return mid;
-    }
-    else if(nums[mid]>target){
-        e=mid-1;
-    }
-    else{
-         s=mid+1;
-    }
-}
-    return -1;
+    int n=nums.size();
+        
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]>target){
+                e=mid-1;
+            }
+            else{
+                s=mid+1;
+            }
+            
+        }
+        return -1;
 }
     int search(vector<int>& nums, int target) {
         int n=nums.size();
-        int pivottt=pivott(nums,n);
-        
-        // line wise code likhna hai for line 1
-        if(target>=nums[pivottt] && target<=nums[n-1]){
-            return bs(nums,target,pivottt,n-1);// bade elemnt wali line
+        int nn=pivott(nums);
+//chote integers ke liye
+        if(target>=nums[nn] && target<=nums[n-1]){
+            return bs(nums,target,nn,n-1);
         }
         else{
-           return bs(nums,target,0,pivottt-1);
-
+            return bs(nums,target,0,nn-1);
         }
-        //return pivottt;
     }
 };
