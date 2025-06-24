4fc966c796a30bce5d021324de7605bd757983a6
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-06-24 20:38:40 +0530

    Time: 23 ms (78.6%), Space: 60.1 MB (92.22%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-    
-    int length=1;
-        int karan=1;
         int n=nums.size();
         if(n==0){
             return 0;
         }
+        int maxx=1;
+        int ans=1;
         sort(nums.begin(),nums.end());
         for(int i=1;i<n;i++){
             if(abs(nums[i]-nums[i-1])==1){
-                length++;
+                maxx++;
             }
-            else if(nums[i]-nums[i-1]==0){
-                
+            else if(abs(nums[i]-nums[i-1])==0){
+
             }
             else{
-                length=1;
+                maxx=1;
             }
-            karan=max(karan,length);
+           ans=max(ans,maxx); 
+
         }
-        return karan;
+        return ans;
     }
 };
