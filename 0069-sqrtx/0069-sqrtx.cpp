361a2fd76b947b444c329260e5aa90c6be8cba70
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-06-28 00:29:57 +0530

    Time: 0 ms (100%), Space: 8.5 MB (85.39%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
     int mySqrt(int x) {
-      long long int start=0;
-        long long int end=x;
-        long long int ans=-1;
-        while(start<=end){
-            long long int mid=start+(end-start)/2;
-            long long int k=mid*mid;;
-            if(k==x){
+        int start = 0;
+        int end = x;
+        int ans = -1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            long long sq = 1LL * mid * mid;
+
+            if (sq == x) {
                 return mid;
+            } else if (sq < x) {
+                ans = mid;
+                start = mid + 1;
+            } else {
+                end = mid - 1;
             }
-            else if(k>x){
-                end=mid-1;
-            }
-            else{
-                ans=mid;
-                start=mid+1;
-
-            }
-            mid=start+(end-start)/2;
         }
-        return ans;
-        
 
+        return ans;
     }
 };
