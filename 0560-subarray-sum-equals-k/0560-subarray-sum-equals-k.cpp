68ecf90e5b6cacb3ce81d0365a91cbfbcf488469
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-06-27 19:25:53 +0530

    Time: 31 ms (97.18%), Space: 45.6 MB (45.59%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int count = 0, sum = 0;
-        unordered_map<int, int> prefixSum; // Map to store cumulative sum and its frequency
-        prefixSum[0] = 1; // Initialize with 0 sum to handle subarrays starting from index 0
-
-        for (int num : nums) {
-            sum += num; // Compute the cumulative sum
-            
-            // Check if `sum - k` exists in the map
-            if (prefixSum.find(sum - k) != prefixSum.end()) {
-                count += prefixSum[sum - k]; // Increment count by the frequency of `sum - k`
+        int n=nums.size();
+        unordered_map<int,int> karan;
+        karan[0]=1;
+        int sum=0;
+        int cnt=0;
+        for(int num: nums){
+            sum+=num;
+            if(karan.find(sum-k)!=karan.end()){
+                
+                cnt+=karan[sum-k];
             }
-
-            // Update the map with the current sum
-            prefixSum[sum]++;
+            karan[sum]++;
         }
-
-        return count;
+        return cnt;
     }
 };
