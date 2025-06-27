2dcf0195b08c84feb7b99f114d360d42f8af01e2
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-06-27 20:46:39 +0530

    Time: 27 ms (3.82%), Space: 31.8 MB (7.21%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
+        map<int,int> karan;
         int n=nums.size();
-        map<int,int> mpp;
         for(int i=0;i<n;i++){
-            mpp[nums[i]]++;
+            karan[nums[i]]++;
         }
-        int ans=-1;
-        for(auto it=mpp.begin();it!=mpp.end();it++){
-            int key=it->first;
-            int value=it->second;
-            if(value==1){
-                ans=key;
+        int index=-1;
+        for(auto& it:karan){
+            int num=it.first;
+            int freq=it.second;
+            if(freq==1){
+                index=num;
             }
         }
-    return ans;    
+      return index;  
     }
 };
