8af506875bac708de60ce54f85517c3b0e488f7f
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-06-27 20:28:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,36 +1,43 @@
 class Solution {
 public:
-    bool search(vector<int>& nums, int target) {
-        int start = 0, end = nums.size() - 1;
+    int pivot(vector<int>& nums) {
+        int s = 0;
+        int e = nums.size() - 1;
+        while (s < e) {
+            int mid = s + (e - s) / 2;
 
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-
-            if (nums[mid] == target) return true;
-
-            // If we can't tell the sorted side
-            if (nums[start] == nums[mid] && nums[mid] == nums[end]) {
-                start++;
-                end--;
-            }
-            // Left half is sorted
-            else if (nums[start] <= nums[mid]) {
-                if (nums[start] <= target && target < nums[mid]) {
-                    end = mid - 1;
-                } else {
-                    start = mid + 1;
-                }
-            }
-            // Right half is sorted
+            if (nums[mid] < nums[e]) {
+                e = mid;
+            } 
+            else if (nums[mid] > nums[e]) {
+                s = mid + 1;
+            } 
             else {
-                if (nums[mid] < target && target <= nums[end]) {
-                    start = mid + 1;
-                } else {
-                    end = mid - 1;
-                }
+                // nums[mid] == nums[e], can't decide the side, reduce e
+                e--;
             }
         }
+        return s;  // index of smallest element (pivot)
+    }
 
-        return false;
+    int bs(vector<int>& nums, int target, int s, int e) {
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] > target) e = mid - 1;
+            else s = mid + 1;
+        }
+        return -1;
+    }
+
+    bool search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int p = pivot(nums);
+
+        if (target >= nums[p] && target <= nums[n - 1]) {
+            return bs(nums, target, p, n - 1) != -1;
+        } else {
+            return bs(nums, target, 0, p - 1) != -1;
+        }
     }
 };
