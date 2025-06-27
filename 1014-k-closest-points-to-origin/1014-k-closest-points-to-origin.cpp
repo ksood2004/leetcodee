ae01078ad684793d0f4f59fc1a6a722be9038bc3
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-06-27 16:00:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        priority_queue <pair<int,pair<int,int>>> karan;
-        vector<int> prem;
+        vector<pair<int,int>> karan;
         int n=points.size();
+        priority_queue<int,pair<int,int>> pq;
         for(int i=0;i<n;i++){
-            karan.push({points[i][0]*points[i][0]+points[i][1]*points[i][1],{points[i][0],points[i][1]}}); // jab 2 d array toh aise present krenge
-            if(karan.size()>k){
-                karan.pop();
+            pq.push_back(points[i][0]*points[i][0]+points[i][1]*points[i][1],{point[i][0],points[i][1]});
+            if(pq.size()>k){
+                pq.pop();
             }
+        }
 
+        while(!pq.empty()){
+            auto point=pq.top().second;
+            pq.pop();
+            karan.push_back(point);
         }
-         vector<vector<int>> result; // do vector list bcz ki humhe pair print krzznz hzi 
-        while (!karan.empty()) {
-           
-            auto point = karan.top().second; // {x, y}
-            karan.pop();
-           
-            result.push_back({point.first, point.second});
-        }
-        return result;
-       
-        
-    
-        
     }
 };
