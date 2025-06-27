49e637f327fba1b2b01d54f26f644e6612a92723
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-06-27 17:38:28 +0530

    Time: 35 ms (69.5%), Space: 32.2 MB (72.39%) - LeetHub

--- 0876-hand-of-straights/0876-hand-of-straights.cpp
+++ 0876-hand-of-straights/0876-hand-of-straights.cpp
@@ -3,24 +3,21 @@ public:
     bool isNStraightHand(vector<int>& hand, int groupSize) {
         if (hand.size() % groupSize != 0) return false;
 
-        priority_queue<int, vector<int>, greater<int>> karan;
-        unordered_map<int, int> freq;
-
-        for (int i = 0; i < hand.size(); i++) {
-            freq[hand[i]]++;
-            karan.push(hand[i]);
+        map<int, int> freq; // sorted automatically
+        for (int card : hand) {
+            freq[card]++;
         }
 
-        while (!karan.empty()) {
-            int start = karan.top();
-            karan.pop();
-
-            if (freq[start] == 0) continue;
+        while (!freq.empty()) {
+            int start = freq.begin()->first; // smallest card
 
             for (int i = 0; i < groupSize; i++) {
                 int card = start + i;
                 if (freq[card] == 0) return false;
                 freq[card]--;
+                if (freq[card] == 0) {
+                    freq.erase(card);
+                }
             }
         }
 
