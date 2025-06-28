7aa28bd29fadbd54bea1e27aab67faeee928b7b7
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-06-28 20:19:20 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.cpp
+++ 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-     int small(vector<int>& nums, int k) {
-        int sum = 0;
-        int n = nums.size();
-        for (int i = 0; i < n; i++) {
-            sum += ceil(double(nums[i]) / k);
-        }
-        return sum;
+int smallest(vector<int>& nums, int k){
+    int karan=0;
+    int n=nums.size();
+    int sum=0;
+    for(int i=0;i<n;i++){
+    long long int ans=ceil((double)nums[i]/k);
+        sum+=ans;
     }
-
+    return sum;
+}
     int smallestDivisor(vector<int>& nums, int threshold) {
-        int n = nums.size();
-        int s = 1; 
-        int e = *max_element(nums.begin(), nums.end());
-        int ans = e;
-
-        while (s <= e) {
+        int s=0;
+        int e=*max_element(nums.begin(),nums.end());
+        int ans=0;
+        while(s<=e){
             int mid=s+(e-s)/2;
-            int midn=small(nums,mid);
-            if (midn<=threshold) {
-            e = mid - 1;
+            if(smallest(nums,mid)<=threshold){
+                ans=mid;
+                e=mid-1;
+            }
+            else{
+                s=mid+1;
+            }
         }
-        else {
-            s = mid + 1;
-        }
-    }
-    return s;
+        return ans;
     }
 };
