b1f265f4bbf4539176b7e8d997773c709ba33eb6
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-06-28 17:32:27 +0530

    Time: 68 ms (17.24%), Space: 87.9 MB (12.42%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        int slow = nums[0];
-        int fast = nums[0];
-
-        // Step 1: Detect cycle
-        do {
-            slow = nums[slow];          // Move one step
-            fast = nums[nums[fast]];    // Move two steps
-        } while (slow != fast);
-
-        // Step 2: Find duplicate
-        slow = nums[0];  // Reset slow to the start
-        while (slow != fast) {
-            slow = nums[slow];
-            fast = nums[fast];
+        unordered_map<int, int> mpp;
+        for (int num : nums) {
+            mpp[num]++;  
+            if (mpp[num] > 1) {
+                return num;  
+            }
         }
-
-        return slow;  // Duplicate number
+        
+        return -1; 
     }
 };
