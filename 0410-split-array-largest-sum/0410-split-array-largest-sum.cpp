820336d27c1fca1867109d2d7756e5984392595e
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-06-29 19:31:34 +0530

    Time: 0 ms (100%), Space: 10.3 MB (99.34%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
-bool karan(vector<int>& nums,int n,int k,int maxsum){
-    int current=1;
+bool split(vector<int>& nums,int n,int maxsum,int k){
     int sum=0;
+    int current=1;
     for(int i=0;i<n;i++){
-        if(sum+nums[i]>maxsum){
+        if(nums[i]>maxsum){
+            return false;
+        }
+        else if(sum+nums[i]>maxsum){
             current++;
             sum=nums[i];
             if(current>k){
@@ -19,18 +22,19 @@ bool karan(vector<int>& nums,int n,int k,int maxsum){
 }
     int splitArray(vector<int>& nums, int k) {
         int n=nums.size();
+        int s=*max_element(nums.begin(),nums.end());
         int sum=0;
-        int maxx=0;
         for(int i=0;i<n;i++){
             sum+=nums[i];
-            maxx=max(maxx,nums[i]);
         }
-        int s=maxx;
         int e=sum;
         int ans=-1;
+        if(k>n){
+            return -1;
+        }
         while(s<=e){
             int mid=s+(e-s)/2;
-            if(karan(nums,n,k,mid)){
+            if(split(nums,n,mid,k)){
                 ans=mid;
                 e=mid-1;
             }
@@ -38,6 +42,6 @@ bool karan(vector<int>& nums,int n,int k,int maxsum){
                 s=mid+1;
             }
         }
-return ans;
+        return ans;
     }
 };
