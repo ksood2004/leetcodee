712511b8dcb97fe3026c7434ec9115c2aead9056
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-07-03 20:08:44 +0530

    Time: 9 ms (49.27%), Space: 11.7 MB (80%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -12,16 +12,18 @@ public:
         if(head==NULL){
             return false;
         }
-    ListNode* slow= head;
-    ListNode* fast= head;
-    while(fast!=NULL && fast->next!=NULL){
-        fast=fast->next->next;
-        slow=slow->next;
-        if(fast==slow){
-            return true;
-        }
-    }
-    return false;
+        ListNode* fast=head;
+        ListNode* slow=head;
 
+        while(fast!=NULL && fast->next!=NULL){
+            fast=fast->next->next;
+            slow=slow->next;
+            if(slow==fast){
+                return true;
+            }
+
+        }
+        return false;
     }
+
 };
