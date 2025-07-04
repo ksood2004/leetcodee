6eff48d1e174477c97fddb30e2858557f910be66
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-07-04 16:32:09 +0530

    Time: 0 ms (100%), Space: 8.5 MB (36.44%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-int t[46];
-int karan(int n){
-    t[0]=1;
-    t[1]=1;
-    for(int i=2;i<=n;i++){
-       t[i]=t[i-1]+t[i-2];
-    }
-    return t[n];
-}
     int climbStairs(int n) {
-        
-        memset(t,0,sizeof(t));
-        return karan(n);
+        if(n==0 || n==1){
+            return 1;
+        }
+        vector<int> karan(n+1);
+        karan[0]=1;
+        karan[1]=1;
+        for(int i=2;i<=n;i++){
+            karan[i]=karan[i-1]+karan[i-2];
+            
+        }
+        return karan[n];
     }
 };
