fb2ec31bb70ff749f963908a815a3f04175441fb
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-07-04 20:47:54 +0530

    Time: 488 ms (5.02%), Space: 57.1 MB (46.71%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,45 +1,40 @@
 class Solution {
 public:
-bool t[201][40001];
-  bool karan(int n,int sum,vector<int>& arr){
-      for(int i=0;i<=n;i++){
-          for(int j=0;j<=sum;j++){
-              if(i==0){
-              t[i][j]=false;
-          }
-          if(j==0){
-              t[i][j]=true;
-          }
-        }
-    }
-          
-          for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= sum; j++) {
-            if (arr[i - 1] <= j) {
-                t[i][j] = t[i - 1][j - arr[i - 1]] || t[i - 1][j];
-            } 
-            else {
-                t[i][j] = t[i - 1][j];
+bool t[1001][40001];
+bool karan(vector<int>& nums,int sum,int n){
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<=sum;j++){
+            if(i==0){
+                t[i][j]=false;
+            }
+            if(j==0){
+                t[i][j]=true;
             }
         }
-   }
-  
+    }
 
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=sum;j++){
+            if(nums[i-1]<=j){
+                t[i][j]=t[i-1][j-nums[i-1]] || t[i-1][j];
+            }
+            else{
+                t[i][j]=t[i-1][j];
+            }
+        }
+    }
     return t[n][sum];
 }
-
     bool canPartition(vector<int>& nums) {
-        int n = nums.size();
-        int summ = 0;
-
-        for (int i = 0; i < n; i++) {
-            summ += nums[i];
+        memset(t,0,sizeof(t));
+        int n=nums.size();
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
         }
-
-        if (summ % 2 != 0) {
+        if(sum%2!=0){
             return false;
         }
-
-        return karan(n, summ / 2, nums);
+        return karan(nums,sum/2,n);
     }
 };
