f061ee576743af7252b47207e1859b8c013a8eb0
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-07-06 19:15:51 +0530

    Time: 0 ms (100%), Space: 14.8 MB (91.73%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -1,34 +1,45 @@
 class Solution {
 public:
+    ListNode* reversee(ListNode* head) {
+        if (head == NULL) {
+            return NULL;
+        }
+        ListNode* prev = NULL;
+        ListNode* temp = head;
+        while (temp != NULL) {
+            ListNode* front = temp->next;
+            temp->next = prev;
+            prev = temp;
+            temp = front;
+        }
+        return prev;
+    }
+
     ListNode* removeNthFromEnd(ListNode* head, int n) {
         if (head == NULL) {
             return NULL;
         }
 
-        int cnt = 0;
-        ListNode* temp = head;
+        int cnt = 1;
+        ListNode* revHead = reversee(head);
+        ListNode* temp = revHead;
+        ListNode* prev = NULL;
+
         while (temp != NULL) {
+            if (cnt == n) {
+                if (prev == NULL) {
+                    revHead = temp->next;  // removing the first node
+                } else {
+                    prev->next = temp->next;
+                }
+                delete temp;
+                break;
+            }
             cnt++;
-            temp = temp->next;
-        }
-        if (cnt == n) {
-            ListNode* temp = head;
-            head = head->next;
-            delete temp;
-            return head;
-        }
-
-        temp = head;
-        int pos = cnt - n; 
-        for (int i = 1; i < pos; i++) {
+            prev = temp;
             temp = temp->next;
         }
 
-      
-        ListNode* nodeToDelete = temp->next;
-        temp->next = temp->next->next;
-        delete nodeToDelete;
-
-        return head;
+        return reversee(revHead);  // <- fix: reverse modified list, not original head
     }
 };
