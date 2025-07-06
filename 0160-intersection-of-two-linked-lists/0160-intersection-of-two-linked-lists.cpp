770dac23f64362d2d9b0ff7379f23eec0de1426f
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-07-06 19:30:31 +0530

    Time: 32 ms (87.98%), Space: 18.5 MB (81.68%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -12,21 +12,24 @@ public:
         if(headA==NULL || headB==NULL){
             return NULL;
         }
-        ListNode* temp1=headA;        
+        ListNode* temp1=headA;
         ListNode* temp2=headB;
+
         while(temp1!=temp2){
             temp1=temp1->next;
             temp2=temp2->next;
             if(temp1==temp2){
                 return temp1;
             }
-            if(temp1 == NULL){
+            if(temp1==NULL){
                 temp1=headB;
             }
             if(temp2==NULL){
                 temp2=headA;
             }
         }
-      return temp1;  
+        return temp1;
+        
+
     }
 };
