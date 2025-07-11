3cef3388e6b210b609b0544b2a47af12b520fb85
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-07-11 18:49:19 +0530

    Time: 47 ms (75.13%), Space: 14.2 MB (73.06%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,33 +1,17 @@
 class Solution {
 public:
-int karann(vector<int>& nums){
-    int n=nums.size();
-    vector<int> karan=nums;
-    sort(karan.begin(),karan.end());
-    karan.erase(unique(karan.begin(), karan.end()), karan.end());
-    int m=karan.size();
-    vector<vector<int>> t(n+1,vector<int> (m+1,0));
-    for(int i=0;i<=n;i++){
-        for(int j=0;j<=m;j++){
-            if(i==0 || j==0){
-                t[i][j]=0;
-            }
-        }
-    }
-
-     for(int i=1;i<=n;i++){
-        for(int j=1;j<=m;j++){
-            if(nums[i-1]==karan[j-1]){
-                t[i][j]=1+t[i-1][j-1];
-            }
-            else{
-                t[i][j]=max(t[i-1][j],t[i][j-1]);
-            }
-        }
-    }
-    return t[n][m];
-}
     int lengthOfLIS(vector<int>& nums) {
-        return karann(nums);
+         int n = nums.size();
+        vector<int> dp(n, 1); // dp[i] = length of LIS ending at index i
+
+        for (int i = 1; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        return *max_element(dp.begin(), dp.end());
     }
 };
