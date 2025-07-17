fa0ff1dd0b91e2e8961338eeb7c7dd4b69e0a7db
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-07-17 20:02:54 +0530

    Time: 0 ms (100%), Space: 13.8 MB (99.27%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        int n = candidates.size();
-        vector<vector<vector<int>>> dp(target + 1);
+        vector<vector<int>> res;
+        vector<int> comb;
+        backtrack(candidates, target, 0, comb, res);
+        return res;
+    }
 
-        dp[0].push_back({});
-
-        for (int num : candidates) {
-            for (int t = num; t <= target; ++t) {
-                for (auto comb : dp[t - num]) {
-                    comb.push_back(num);
-                    dp[t].push_back(comb);
-                }
-            }
+private:
+    void backtrack(vector<int>& candidates, int target, int idx, vector<int>& comb, vector<vector<int>>& res) {
+        if (target == 0) {
+            res.push_back(comb);
+            return;
         }
 
-        return dp[target];
+        if (target < 0 || idx == candidates.size()) {
+            return;
+        }
+
+        // Include current number
+        comb.push_back(candidates[idx]);
+        backtrack(candidates, target - candidates[idx], idx, comb, res);  // stay on same idx for unbounded use
+        comb.pop_back();
+
+        // Exclude current number
+        backtrack(candidates, target, idx + 1, comb, res);
     }
 };
