590dfca0f6ac56514e4321fa7c2951e035e84993
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-08-27 23:57:45 +0530

    Time: 3 ms (70.55%), Space: 14.9 MB (36.15%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n=nums.size();
-        unordered_map<int,int> karan;
+        map<int,int> karan;
+       
         for(int i=0;i<n;i++){
             int k=target-nums[i];
             if(karan.find(k)!=karan.end()){
                 return {karan[k],i};
             }
             karan[nums[i]]=i;
-
         }
-    return {};  
+       return {}; 
     }
 };
