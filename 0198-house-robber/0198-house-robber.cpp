c7e068de85ea354cb149217d02d1a667a816fad7
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-08-28 22:55:06 +0530

    Time: 0 ms (100%), Space: 10.8 MB (22.7%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
-    int karn(vector<int>& karan) {
-        int n = karan.size();
-        if (n == 0) return 0;
-        if (n == 1) return karan[0];
-
-        vector<int> t(n);
-        t[0] = karan[0];
-        t[1] = max(karan[0], karan[1]);
-
-        for (int i = 2; i < n; i++) {
-            t[i] = max(t[i - 1], t[i - 2] + karan[i]);
-        }
-        return t[n - 1];
-    }
-
     int rob(vector<int>& nums) {
-        return karn(nums);
+        int n = nums.size();
+        if(n == 0) return 0;
+        if(n == 1) return nums[0];
+
+        vector<int> t(n, 0);
+        t[0] = nums[0];
+        t[1] = max(nums[0], nums[1]);
+
+        for(int i = 2; i < n; i++){
+            t[i] = max(t[i-1], t[i-2] + nums[i]);
+        }
+        return t[n-1];
     }
 };
