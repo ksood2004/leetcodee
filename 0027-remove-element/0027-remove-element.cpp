aa35f588cba473160fe2c39eb87be1e4f09e4085
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-08-31 13:32:26 +0530

    Time: 0 ms (100%), Space: 11.8 MB (45.17%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -2,14 +2,15 @@ class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
         int n=nums.size();
-        int karan=0;
+        //vector<int> karan;
+        //sort(nums.begin(),nums.end());
+        int index=0;
         for(int i=0;i<n;i++){
             if(nums[i]!=val){
-                nums[karan++]=nums[i];
+                nums[index]=nums[i];
+                index++;
             }
-
         }
-        return karan;
-        
+        return index;
     }
 };
