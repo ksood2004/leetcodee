dec4f0efc42ff1ca9ef63a18f80342bb3f5bd997
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-08-31 13:24:55 +0530

    Time: 0 ms (100%), Space: 12.2 MB (70.51%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,46 +1,28 @@
 class Solution {
 public:
-    void swapp(vector<int>& nums1, vector<int>& nums2, int idx1, int idx2) {
-        if (nums1[idx1] > nums2[idx2]) {
-            swap(nums1[idx1], nums2[idx2]);
-        }
-    }
-
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int k = m + n;
-        int gap = (k / 2) + (k % 2);
-
-        while (gap > 0) {
-            int i = 0;
-            int j = i + gap;
-
-            while (j < k) {
-                // arr1 and arr1
-                if (i < m && j < m) {
-                    swapp(nums1, nums1, i, j);
-                }
-                // arr1 and arr2
-                else if (i < m && j >= m) {
-                    swapp(nums1, nums2, i, j - m);
-                }
-                // arr2 and arr2
-                else {
-                    swapp(nums2, nums2, i - m, j - m);
-                }
-
+        int i = 0, j = 0;
+        vector<int> nums;
+        while (i < m && j < n) {
+            if (nums1[i] <= nums2[j]) {
+                nums.push_back(nums1[i]);
                 i++;
+            } else {
+                nums.push_back(nums2[j]);
                 j++;
             }
-
-            if (gap == 1) {
-                break;
-            }
-            gap = (gap / 2) + (gap % 2);
         }
 
-        // Copy sorted nums2 back into nums1
-        for (int i = 0; i < n; i++) {
-            nums1[m + i] = nums2[i];
+        while (i < m) {
+            nums.push_back(nums1[i]);
+            i++;
+        }
+        while (j < n) {
+            nums.push_back(nums2[j]);
+            j++;
+        }
+        for (int k = 0; k < m + n; k++) {
+            nums1[k] = nums[k];
         }
     }
 };
