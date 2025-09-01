0cd863c116a451a109137b20d98e42e6f8645952
KARAN SOOD <114595853+ksood2004@users.noreply.github.com>
2025-09-01 17:09:33 +0530

    Time: 0 ms (100%), Space: 23.4 MB (21.06%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -2,23 +2,24 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n=ratings.size();
-        vector<int> karan(n,1); // for left
-        vector<int> aaryan(n,1); // for right;
+        vector<int> candy1(n,1);
+        vector<int> candy2(n,1);
         for(int i=1;i<n;i++){
             if (ratings[i] > ratings[i - 1]) {
-                karan[i] = karan[i - 1] + 1;
+                candy1[i] = candy1[i - 1] + 1;
             }
         }
+       
         for(int i=n-2;i>=0;i--){
-            if(ratings[i]>ratings[i+1]){
-                aaryan[i]=aaryan[i+1]+1;
+            if(ratings[i] > ratings[i +1]){
+                candy2[i]=candy2[i+1]+1;
+
             }
         }
-       int sum = 0;
-        for (int i = 0; i < n; i++) {
-            sum += max(karan[i], aaryan[i]);
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=max(candy1[i],candy2[i]);
         }
-
         return sum;
     }
 };
